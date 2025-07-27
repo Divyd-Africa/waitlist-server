00cd7421c3a59c2f4904e633ab468e4a0cc6c6c3
0layimika <olakay739@gmail.com>
2025-07-27 20:55:15 +0100

    added render url to allowed hosts

--- Waitlist/urls.py
+++ Waitlist/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('waiting.urls')),  # Make waiting app the root
+    path('/waitlist', include('waiting.urls')),  # Make waiting app the root
 ]
